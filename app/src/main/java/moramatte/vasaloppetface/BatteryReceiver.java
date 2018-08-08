d6bbf82f840c75c600cceb5add7e0cb15c122e29
Moramatte <moramatte@gmail.com>
2018-08-08 15:18:51 +0200

    Initial

---
+++ app/src/main/java/moramatte/vasaloppetface/BatteryReceiver.java
@@ -0,0 +1,14 @@
+package moramatte.vasaloppetface;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatteryReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int j = 0;
+        j++;
+
+    }
+}
